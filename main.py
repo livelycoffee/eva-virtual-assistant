34db252734610e11be00689e70d19e43302671e3
unknown <khanakbhojwani@gmail.com>
2026-04-04 22:33:41 +0530

    Go Away Change in main

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def run_eva():
             continue
 
         sp_engine.speak(get_llm_response(query))
-        if "exit" in query or "bye" in query or "shut down" in query or "shutdown" in query or "goodbye" in query:
+        if "exit" in query or "bye" in query or "shut down" in query or "shutdown" in query or "goodbye" or "go away" in query:
             listener.shutdown()
             exit(0)
             break # --> Just in case
